1fae1b890dabb017ba76d0a499b2cc1f0d0632c1
Daniil <danalatanov@gmail.com>
2024-03-10 18:28:56 +0300

    hw4

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,17 +1,26 @@
 package edu.java.scrapper;
 
+import liquibase.Liquibase;
+import liquibase.database.core.PostgresDatabase;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Testcontainers
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:15")
+        POSTGRES = new PostgreSQLContainer<>("postgres:16")
             .withDatabaseName("scrapper")
             .withUsername("postgres")
             .withPassword("postgres");
@@ -20,8 +29,29 @@ public abstract class IntegrationTest {
         runMigrations(POSTGRES);
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+    @SuppressWarnings("deprecation")
+    private static void runMigrations(JdbcDatabaseContainer<?> container) {
+        Path migrations = new File("").toPath().toAbsolutePath().getParent().resolve("migrations");
+
+        try {
+            Connection connection =
+                DriverManager.getConnection(
+                    container.getJdbcUrl(),
+                    container.getUsername(),
+                    container.getPassword()
+                );
+            ResourceAccessor changelogDirectory = new DirectoryResourceAccessor(migrations);
+            PostgresDatabase db = new PostgresDatabase();
+            db.setConnection(new JdbcConnection(connection));
+
+            Liquibase liquibase = new liquibase.Liquibase("master.xml", changelogDirectory, db);
+            liquibase.update("");
+            liquibase.close();
+            changelogDirectory.close();
+            connection.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
